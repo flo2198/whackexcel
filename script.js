06129c77809463fffd43665aa737196bcd4c6b20
flo2198 <floriankaiser98@web.de>
2023-10-07 19:02:20 +0200

    working clicking backwards issue

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const targets = document.querySelectorAll('.target');
 let lastHole;
 let timeUp = false;
 let score = 0;
-let showing = 'init'
+let showing = 'none'
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -15,37 +15,44 @@ function randomHole(holes) {
     if (idx === lastHole) {
         return randomHole(holes);
     }
-    lastHole = idx;
+    this.lastHole = idx;
     return idx;
 }
 
-function randomPeeper(hole) {
+function randomPeeper(hole, target) {
     const decider = Boolean(Math.round(Math.random() * 0.6));
+    hole.classList.add('up');
     if (decider) {
-        hole.classList.add('up', 'neo');
+        target.classList.add('neo');
         showing = 'neo';
     } else {
-        hole.classList.add('up', 'excel');
+        // hole.classList.add('up');
+        target.classList.add('excel');
         showing = 'excel';
     }
     return decider;
 }
 
-function antiPeep(decider, hole) {
+function antiPeep(decider, hole, target) {
+    hole.classList.remove('up');
     if (decider) {
-        hole.classList.remove('up', 'neo');
+        target.classList.remove('neo');
     } else {
-        hole.classList.remove('up', 'excel');
+        target.classList.remove('excel');
+        // hole.classList.remove('up', 'excel');
     }
 }
 
 function peep() {
-    const time = randomTime(1000, 1100); 
-    const id = randomHole(holes);
-    const hole = holes[id]
-    const decider = randomPeeper(hole);
+    const time = randomTime(400, 1000); 
+    const id = 3;// randomHole(holes);
+    const hole = holes[id];
+    console.log(hole);
+    console.log(lastHole);
+    const target = targets[id];
+    const decider = randomPeeper(hole, target);
     setTimeout(() => {
-        antiPeep(decider, hole);
+        antiPeep(decider, hole, target);
         if (!timeUp) peep();
     }, time);
 }
@@ -56,18 +63,33 @@ function startGame() {
     score = 0;
     peep();
     setTimeout(() => timeUp = true, 10000);
+    showing = 'none';
 }
 
 // Catch hit
 function bonk(e) {
-    console.log('Been hit!')
-    if (!e.isTrusted) return; // cheater!
-    if (showing === 'excel') {
-        this.parentNode.classList.remove('up', 'excel');
-        score++;
-    }
-    else if (showing === 'neo') {
-        this.parentNode.classList.remove('up', 'neo');
+    let targetVal = e.target.classList[1];
+    let hitId = targetVal.substring(6);
+    console.log(hitId);
+    console.log(lastHole);
+    if (hitId == lastHole) {
+        if (!e.isTrusted) {
+            console.log('CHEATER!'); 
+            return; // cheater!
+        }
+        if (showing == 'excel') {
+            this.parentNode.children[0].classList.remove('excel');
+            this.parentNode.classList.remove('up');
+            score++;
+        }
+        else if (showing == 'neo') {
+            this.parentNode.children[0].classList.remove('neo');
+            this.parentNode.classList.remove('up');
+            score--;
+        }
+    } else if (showing == 'none') {
+        return;
+    } else {
         score--;
     }
     scoreBoard.textContent = score;
