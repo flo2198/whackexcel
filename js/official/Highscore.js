d5732faaaecaf86254ec6f01476d0e24cfee274d
flo2198 <86657546+flo2198@users.noreply.github.com>
2023-10-13 14:28:13 +0200

    Create Highscore.js

---
+++ js/official/Highscore.js
@@ -0,0 +1,21 @@
+init = false;
+
+let scoreArray = {
+    char: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true],
+    duration: [820, 469, 950, 926, 412, 853, 964, 906, 664, 510, 709, 948, 477, 578, 733, 961, 639, 987, 959, 868, 828, 843, 528, 571, 858, 888, 471, 577, 712, 945, 746, 967, 496, 617, 427, 762, 741, 833, 508, 426]
+}
+
+function initOfficalGame () {
+    let trialArray = scoreArray;
+    init = true;
+}
+
+export function pickHigh () {
+    let arrayLength = trialArray.char.length;
+    let randomNum = Math.floor(Math.random() * arrayLength);
+    let randomChar = trialArray.char.splice(randomNum, 1)[0];
+    let randomDur = trialArray.duration.splice(randomNum, 1)[0];
+    return [randomDur, randomChar];
+}
+
+initOfficalGame()
