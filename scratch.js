c53c3d4ffdab52434baf19c322a9df0f8fcc8763
flo2198 <86657546+flo2198@users.noreply.github.com>
2023-10-13 14:31:49 +0200

    Delete scratch.js

--- scratch.js
+++
@@ -1,9 +0,0 @@
-function randomPeeper() {
-    let doof = Math.random();
-    console.log(doof);
-    let decider = Math.round(doof);
-    console.log(decider)
-    console.log(Boolean(decider))
-}
-
-randomPeeper()
