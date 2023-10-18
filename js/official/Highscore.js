68317362d6bb19e4b25bfba10445ced409685ad5
flo2198 <86657546+flo2198@users.noreply.github.com>
2023-10-18 22:24:12 +0200

    Update Highscore.js to work flawless

--- js/official/Highscore.js
+++ js/official/Highscore.js
@@ -1,28 +1,21 @@
-let init = false;
-let trialArray;
+let trialChar;
+let trialDuration
 
-let scoreArray = {
-    char: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true],
-    duration: [820, 469, 950, 926, 400, 853, 964, 906, 664, 556, 709, 948, 777, 578, 753, 1000, 639, 987, 959, 868, 818, 843, 666, 571, 858, 969, 471, 577, 712, 945, 746, 967, 696, 617, 492, 762, 741, 833, 570, 420]
-}
-
-/*
-let scoreArray = {
-    char: [false, true],
-    duration: [2000, 10000]
-}
-*/
+const scoreChar = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true];
+const scoreDuration = [820, 469, 950, 926, 400, 853, 964, 906, 664, 556, 709, 948, 777, 578, 753, 1000, 639, 987, 959, 868, 818, 843, 666, 571, 858, 969, 471, 577, 712, 945, 746, 967, 696, 617, 492, 762, 741, 833, 570, 420]; 
 
 function initOfficalGame () {
-    trialArray = scoreArray;
-    init = true;
+    console.log('initting official');
+    trialChar = [...scoreChar];
+    trialDuration = [...scoreDuration];
 }
 
 export function pickNext () {
-    let arrayLength = trialArray.char.length;
+    let arrayLength = trialChar.length;
     let randomNum = Math.floor(Math.random() * arrayLength);
-    let randomChar = trialArray.char.splice(randomNum, 1)[0];
-    let randomDur = trialArray.duration.splice(randomNum, 1)[0];
+    let randomChar = trialChar.splice(randomNum, 1)[0];
+    let randomDur = trialDuration.splice(randomNum, 1)[0];
+    if (trialChar.length == 0) initOfficalGame()
     return [randomDur, randomChar];
 }
 
