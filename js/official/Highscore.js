c458d8d00f7b7222aa61a0d524ef14b503bb8dba
flo2198 <86657546+flo2198@users.noreply.github.com>
2023-10-18 21:04:16 +0200

    almost working highscore mode

--- js/official/Highscore.js
+++ js/official/Highscore.js
@@ -1,12 +1,20 @@
-init = false;
+let init = false;
+let trialArray;
 
 let scoreArray = {
     char: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true],
     duration: [820, 469, 950, 926, 412, 853, 964, 906, 664, 510, 709, 948, 477, 578, 733, 961, 639, 987, 959, 868, 828, 843, 528, 571, 858, 888, 471, 577, 712, 945, 746, 967, 496, 617, 427, 762, 741, 833, 508, 426]
 }
 
+/*
+let scoreArray = {
+    char: [false, true],
+    duration: [2000, 10000]
+}
+*/
+
 function initOfficalGame () {
-    let trialArray = scoreArray;
+    trialArray = scoreArray;
     init = true;
 }
 
