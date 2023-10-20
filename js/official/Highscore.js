0cd7f3fecf56e19cf0ae2cbd4f7c867931c0915a
flo2198 <86657546+flo2198@users.noreply.github.com>
2023-10-20 14:51:29 +0200

    highscore and verify

--- js/official/Highscore.js
+++ js/official/Highscore.js
@@ -1,8 +1,8 @@
 let trialChar;
 let trialDuration
 
-const scoreChar = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true];
-const scoreDuration = [820, 469, 950, 926, 400, 853, 964, 906, 664, 556, 709, 948, 777, 578, 753, 1000, 639, 987, 959, 868, 818, 843, 666, 571, 858, 969, 471, 577, 712, 945, 746, 967, 696, 617, 492, 762, 741, 833, 570, 420]; 
+const scoreChar = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2];
+const scoreDuration = [607, 629, 620, 637, 496, 714, 733, 486, 546, 459, 666, 699, 520, 517, 782, 563, 568, 412, 702, 507, 579, 432, 429, 670, 501, 581, 555, 657, 462, 595, 595, 576, 577, 752, 446, 447, 419, 665, 739, 748, 636, 470, 512, 568, 502, 413, 757, 420, 605, 591, 608, 634, 423, 617, 478, 560, 512, 435, 516, 594, 718, 520, 607, 629, 726, 611, 545, 550, 472, 483]; 
 
 function initOfficalGame () {
     console.log('initting official');
---
+++ js/official/verify.js
@@ -0,0 +1,14 @@
+let score;
+let date;
+let name;
+
+export function createVerifier(token = null, score, date, name) {
+    score = score;
+    date = date;
+    name = name;
+    return verifier
+}
+
+export function verify(token) {
+
+}
